75a8431b96a406fe09e8a45b8c3b78496648ad75
wangkun <wangkun@woodemi.com>
2019-01-19 19:52:41 +0800

    Add iosSample
    Signed-off-by: wangkun <wangkun@woodemi.com>

---
+++ iosSample/NotepadKitSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotepadKitSample
+//
+//  Created by SunbreakWang on 2019/1/19.
+//  Copyright © 2019 Woodemi Tech Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
